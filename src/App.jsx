d11c1726a9b8dca20662d3302e3ff8f816ff6466
janarthanan2107 <janarthanan.v2107@gmail.com>
2023-10-25 11:00:01 +0530

    ui fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 // import img
 import friends from "/assets/friends.webp";
 // style
-import "./App.css"
+import "./App.css";
 
 const App = () => {
-  const Cell = () => {
+  const Cell = (num) => {
     return (
       <div className="w-[100px] h-[100px] shadow-sm flex justify-center items-center bg-white font-game text-[2rem]">
         x
@@ -21,36 +21,43 @@ const App = () => {
         </h1>
 
         {/* game section */}
-    <div className="flex justify-center items-center h-[80vh] gap-[8rem]">
-        {/* left */}
-        <div className="w-[500px] relative">
-          {/* message */}
-          <div className="font-speech text-[2rem]">
-            <h3 className="message absolute top-1 -left-5 animate-bounce p-3 bg-lightDarkPurple rounded-lg">It's your turn, player X</h3>
-            {/* <h3 className="message absolute top-1 -right-5 animate-bounce p-3 bg-lightDarkPurple rounded-lg">Now you, player O!</h3> */}
+        <div className="flex justify-center items-center h-[80vh] gap-[8rem]">
+          {/* left */}
+          <div className="w-[500px] relative">
+            {/* message */}
+            <div className="font-speech text-[2rem]">
+              <h3 className="message absolute top-1 -left-5 animate-bounce p-3 bg-lightDarkPurple rounded-lg">
+                It's your turn, player X
+              </h3>
+              {/* <h3 className="message absolute top-1 -right-5 animate-bounce p-3 bg-lightDarkPurple rounded-lg">Now you, player O!</h3> */}
+            </div>
+            <img src={friends} alt="friends" />
           </div>
-          <img src={friends} alt="friends" />
-        </div>
 
-        {/* right */}
-        <div className="xo-grid w-[330px] flex flex-col gap-1 bg-darkPurple p-2">
-          <div className="flex justify-between">
-            <Cell />
-            <Cell />
-            <Cell />
-          </div>
-          <div className="flex justify-between">
-            <Cell />
-            <Cell />
-            <Cell />
-          </div>
-          <div className="flex justify-between">
-            <Cell />
-            <Cell />
-            <Cell />
+          {/* right */}
+          <div className="flex flex-col items-center">
+            <div className="xo-grid w-[330px] flex flex-col gap-1 bg-darkPurple p-2">
+              <div className="flex justify-between">
+                <Cell />
+                <Cell />
+                <Cell />
+              </div>
+              <div className="flex justify-between">
+                <Cell />
+                <Cell />
+                <Cell />
+              </div>
+              <div className="flex justify-between">
+                <Cell />
+                <Cell />
+                <Cell />
+              </div>
+            </div>
+            <button className="px-4 py-3 mt-5 bg-darkPurple text-white rounded-lg">
+              Restart Game
+            </button>
           </div>
         </div>
-    </div>
       </div>
     </>
   );
